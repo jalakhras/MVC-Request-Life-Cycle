d77dacb25d7bb7f48ed88c085ac1cff734b75055
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-03-17 12:02:31 +0200

    Pre Application Start Method

--- MVC Request Life Cycle/Global.asax.cs
+++ MVC Request Life Cycle/Global.asax.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MVC_Request_Life_Cycle;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+[assembly: PreApplicationStartMethod(typeof(MvcApplication), "Register")]
 namespace MVC_Request_Life_Cycle
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void Register()
+        {
+            HttpApplication.RegisterModule(typeof(LogModule));
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
---
+++ MVC Request Life Cycle/LogModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Request_Life_Cycle
+{
+    public class LogModule : IHttpModule
+    {
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.LogRequest += LogEvent;
+
+        }
+
+        private void LogEvent(object src, EventArgs args)
+        {
+            if (HttpContext.Current.CurrentNotification == RequestNotification.LogRequest)
+            {
+                if ((MvcHandler)HttpContext.Current.Handler != null)
+                {
+                    Debug.WriteLine("This was logged!");
+                }
+
+            }
+        }
+    }
+}
