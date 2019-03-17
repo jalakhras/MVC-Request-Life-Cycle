e6054e339588c297d3f429dd8275044bb1a4174a
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-03-17 12:24:13 +0200

    Explain event life cycle

--- MVC Request Life Cycle/Global.asax.cs
+++ MVC Request Life Cycle/Global.asax.cs
@@ -23,32 +23,58 @@ namespace MVC_Request_Life_Cycle
         }
         protected void Application_BeginRequest()
         {
-            Debug.WriteLine("Begin Request this Exeute when Begin Requst");
+            Debug.WriteLine("Begin Request this Exeute when Begin Requst." +
+                "Application_BeginRequest is an event handler.  It is part of the ASP.NET website system." +
+                " The Application_BeginRequest method is executed on all requests handled by the ASP.NET runtime." +
+                " Occurs as the first event in the HTTP pipeline chain of execution when ASP.NET responds to a request. The BeginRequest event signals the creation of any given new request." +
+                "This event is always raised and is always the first event to occur during the processing of a request");
+
+
         }
+        protected void Application_AuthenticateRequest()
+        {
+            Debug.WriteLine("Authenticate Request " +
+                "The AuthenticateRequest event signals that the configured authentication mechanism has authenticated the current request." +
+                "Subscribing to the AuthenticateRequest event ensures that the request will be authenticated before processing the attached module or event handle.");
+
+        }
+        protected void Application_AuthorizeRequest()
+        {
+            Debug.WriteLine("Authorize Request " +
+                "The AuthorizeRequest event signals that ASP.NET has authorized the current request." +
+                "You can subscribe to the AuthorizeRequest event to perform custom authorization.");
+
+        }
+
 
         protected void Application_MapRequestHandler()
         {
-            Debug.WriteLine("Map Handler this Exeute After End  Begin Requst to get map Requst");
+            Debug.WriteLine("Map Handler this Exeute After End  Begin Requst to get map Requst" +
+                "The MapRequestHandler event is used by the ASP.NET infrastructure to determine the request handler for the current request based on the file-name extension of the requested resource.");
+
         }
 
         protected void Application_PostMapRequestHandler()
         {
-            Debug.WriteLine("Post Map Handler");
+            Debug.WriteLine("Post Map Handler , Occurs when ASP.NET has mapped the current request to the appropriate HTTP handler");
         }
 
         protected void Application_AcquireRequestState()
         {
-            Debug.WriteLine("Request State");
+            Debug.WriteLine("Request State ," +
+                "Occurs when ASP.NET acquires the current state (for example, session state) that is associated with the current request. A valid session ID must exist.");
         }
 
         protected void Application_PreRequestHandlerExecute()
         {
-            Debug.WriteLine("Pre Handler Execute After this compiler execute Action (Requset)");
+            Debug.WriteLine("Pre Handler Execute brfore  this compiler execute Action (Requset) ," +
+                "Occurs just before ASP.NET starts executing an event handler.");
         }
 
         protected void Application_PostRequestHandlerExecute()
         {
-            Debug.WriteLine("Post Handler Execute Note : this execute After execute Action Methods ");
+            Debug.WriteLine("Post Handler Execute Note : this execute After execute Action Methods," +
+                "Occurs when the ASP.NET event handler has finished generating the output ");
         }
 
         protected void Application_EndRequest()
